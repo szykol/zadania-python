685c1232e7833b039f7c6a309c4bef7c33b7ed9a
szykol <szymek554@gmail.com>
2019-03-12 01:14:50 +0100

    Dodano skrypt naprawiajacy ogonki w plikach polecenie.md
    Polecenia przekopiowywane z plikow pdf maja niepoprawne znaki w
    miejscu ą i ę. Skrypt ten zamienia niepoprawne sekwencje znakow
    na poprawne znaki z ogonkami

---
+++ ogonki.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# skrypt naprawiający ogonki w plikach 'polecenie.md'
+import argparse
+import signal
+import sys
+import re
+
+def ctr_c_handler(sig, frame):
+    sys.exit(0)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, ctr_c_handler)
+
+    parser = argparse.ArgumentParser(description='Naprawia ogonki w plikach polecenie.md')
+    parser.add_argument('file', help='The file containing binary source code', type=str)
+
+    args = parser.parse_args()
+    filename = args.file
+    
+    ogonki = {
+        'a': 'ą',
+        'e': 'ę'
+    }
+
+    ex = ' ˛[ae]'
+    ext = filename.rfind('.')
+    with open(filename, 'r', encoding="utf8") as f, open(f'{filename[:ext]}_ogonki.md', 'w', encoding="utf8") as fout: 
+        for line in f:
+            while True:
+                m = re.search(ex, line)
+                if m is None:
+                    break
+                
+                i = m.start()
+                line = line[:i] +  ogonki[line[i+2]] + line[m.end():]
+
+            fout.write(line)
+
+
