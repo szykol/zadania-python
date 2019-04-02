a2f49f4497dfc474459d1fcfc7579a73a3050076
szykol <szymonkolton@gmail.com>
2019-04-02 20:02:06 +0200

    Fix zadania 3.2
    Zmiana metody select na przyjmujaca obiekty iterowalne a nie generatory
    Zmiana tworzenia trojek pitagorejskich z funkcji generatora na
    wyrazenie generatora

--- zadania/3.2/main.py
+++ zadania/3.2/main.py
@@ -3,42 +3,28 @@ def gen_calk():
     l = 1
     while True:
         yield l
-        l += 1
+        l += 1 
 
 def kwadraty():
     ''' Generuje kwadraty dodatnich liczb calkowitych zwiekszanych o 1 '''
 
     for c in gen_calk():
-        yield c ** 2
+        yield c ** 2 
 
-def select(gen, n):
-    ''' Tworzy n-elementową listę wartości dowolnego obiektu iterowalnego 
-        Wersja bez bezpośredniego używania iter() i next() '''
-
-    import itertools
-    return list(itertools.islice(gen(), n))
-
-def select_it(gen, n):
+def select(iterowalny, n):
     ''' Tworzy n-elementową listę wartości dowolnego obiektu iterowalnego '''
-    it = iter(gen())
+    it = iter(iterowalny)
 
     l = []
     for _ in range(n):
-        l.append(next(it))
+        try:
+            l.append(next(it))
+        except StopIteration:
+            break
 
     return l
 
-def trojki():
-    ''' Generuje trójki pitagorejskie '''
-    c = 1
-    while True:
-        for b in range(1, c):
-            for a in range(1, b):
-                if a*a + b*b == c*c:
-                    yield (a,b,c)
-                    
-        c += 1
-
+trojki = ((a, b, c) for c in gen_calk() for b in range(1, c) for a in range(1, b) if a**2 + b**2 == c**2)
 
 print(select(trojki, 15))
 
